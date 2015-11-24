78811b5bd1c5a602051ef450102d0db45aa444e2
Salma BOUGACHA <sb238920@is223203.intra.cea.fr>
2015-11-24 17:58:13 +0100

    masked correct basic cbf

--- procasl/pipelines_examples/single_subject.py
+++ procasl/pipelines_examples/single_subject.py
@@ -11,11 +11,11 @@ mem = Memory('/tmp/no_workflow')
 # Data
 func_file = '/tmp/func.nii'
 anat_file = '/tmp/anat.nii'
+paths = ['/i2bm/local/spm12-standalone-6472/spm12_mcr/spm12/']  # needed ?
 
 # Rescale
 rescale = mem.cache(preprocessing.Rescale)
-out_rescale = rescale(in_file=func_file,
-                      ss_tr=35.4, t_i_1=800., t_i_2=1800.)
+out_rescale = rescale(in_file=func_file, ss_tr=35.4, t_i_1=800., t_i_2=1800.)
 
 # Realign to first scan
 realign = mem.cache(preprocessing.Realign)
@@ -23,7 +23,7 @@ out_realign = realign(
     in_file=out_rescale.outputs.rescaled_file,
     register_to_mean=False,
     correct_tagging=True,
-    paths=['/i2bm/local/spm12-6470/'])
+    paths=paths)
 
 # Compute mean ASL
 average = mem.cache(preprocessing.Average)
@@ -36,7 +36,7 @@ out_segment = segment(
     gm_output_type=[False, False, True],
     wm_output_type=[False, False, True],
     save_bias_corrected=True,
-    paths=['/i2bm/local/spm12-6470/'])
+    paths=paths)
 
 # Coregister anat to mean ASL
 coregister_anat = mem.cache(spm.Coregister)
@@ -46,8 +46,7 @@ out_coregister_anat = coregister_anat(
     apply_to_files=[out_segment.outputs.native_gm_image,
                     out_segment.outputs.native_wm_image],
     write_interp=3,
-    jobtype='estwrite',
-    paths=['/i2bm/local/spm12-6470/'])
+    jobtype='estwrite')
 
 # Get M0
 m0_file = preprocessing.save_first_scan(func_file)
@@ -59,14 +58,14 @@ out_coregister_m0 = coregister_m0(
     source=m0_file,
     write_interp=3,
     jobtype='estwrite',
-    paths=['/i2bm/local/spm12-6470/'])
+    paths=paths)
 
 # Smooth M0
 smooth_m0 = mem.cache(spm.Smooth)
 out_smooth_m0 = smooth_m0(
     in_files=out_coregister_m0.outputs.coregistered_source,
     fwhm=[5., 5., 5.],
-    paths=['/i2bm/local/spm12-6470/'])
+    paths=paths)
 
 # Compute perfusion
 n_scans = preprocessing.get_scans_number(out_realign.outputs.realigned_files)
@@ -78,8 +77,28 @@ perfusion_file = quantification.compute_perfusion(
     tag_scans=tag_scans)
 
 # Compute CBF
+import pdb
+pdb.set_trace()
 quantify = mem.cache(quantification.QuantifyCBF)
 out_quantify = quantify(
     perfusion_file=perfusion_file,
     m0_file=out_smooth_m0.outputs.smoothed_files,
-    tr=2500.)
+    tr=2500.,
+    t1_gm=1331.)
+
+# Mask CBF map with brain mask
+brain_mask_file = preprocessing.compute_brain_mask(
+    out_coregister_anat.outputs.coregistered_source, frac=.2)
+cbf_map = preprocessing.apply_mask(out_quantify.outputs.cbf_file,
+                                   brain_mask_file)
+
+# Plot CBF map on top of anat
+import matplotlib.pylab as plt
+from nilearn import plotting
+
+plotting.plot_stat_map(
+    cbf_map,
+    bg_img=out_coregister_anat.outputs.coregistered_source,
+    threshold=.1, vmax=150.,
+    display_mode='z')
+plt.show()
